56c2affe93b6eb5378787e71326c0c1d5fca9361
AyushSingh4321 <ayushksb11@gmail.com>
2024-07-05 23:51:58 +0530

    addedBasicAuthenticationScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './screens/auth_screen.dart';
 import './screens/chat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -19,8 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ChatScreen(),
+      home: AuthScreen(),
     );
   }
 }
-
