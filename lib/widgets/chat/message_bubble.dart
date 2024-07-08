455dfd221b45c361faa64ed8dfb7d403026584ff
AyushSingh4321 <ayushksb11@gmail.com>
2024-07-09 00:53:33 +0530

    baseChatBubbleWidgetCreated

---
+++ lib/widgets/chat/message_bubble.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MessageBubble extends StatelessWidget {
+  MessageBubble(this.message);
+
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          width: 140,
+          padding: EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 16,
+          ),
+          margin: EdgeInsets.symmetric(
+            vertical: 6,
+            horizontal: 4,
+          ),
+          child: Text(
+            message,
+            style:  TextStyle(color: Colors.white,fontSize: 15)
+          ),
+        ),
+      ],
+    );
+  }
+}
